74317a9213071c7ca24491bf72793c81c3ff866a
Semyon <semenb668@gmail.com>
2025-05-18 16:26:24 +0600

    L42: добавили нестандартные пути к templates и создали первый шаблон thanks.html

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import main
+from core.views import main, master_detail, thanks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
+    path('masters/<int:master_id>/', master_detail),
+    path('thanks/', thanks),
 ]
