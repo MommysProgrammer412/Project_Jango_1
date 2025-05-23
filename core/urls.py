cf5a329db9b9dfb9ac448063ad32b05e9edae75b
Semyon <semenb668@gmail.com>
2025-05-23 17:11:19 +0600

    lesson_44: обновлены шаблоны для страниц деталей заказа и списка заказов с использованием именованных маршрутов. Шаблонный тег URL

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from core.views import master_detail, thanks, test, orders_list, order_detail
 urlpatterns = [
     path('masters/<int:master_id>/', master_detail),
     path('thanks/', thanks),
-    path('orders/', orders_list),
-    path('orders/<int:order_id>/', order_detail),
+    path('orders/', orders_list, name='orders_list'),
+    path('orders/<int:order_id>/', order_detail, name='order_detail'),
 ]
