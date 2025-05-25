11fbc58aefb03a714db2e4d1e009d274df689e7a
Semyon <semenb668@gmail.com>
2025-05-25 18:23:59 +0600

    lesson_46: добавлены собственные фильтры шаблона для форматирования цен и обновлены шаблоны для их использования

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
+# core/urls.py
 from django.contrib import admin
 from django.urls import path
-from core.views import master_detail, thanks, test, orders_list, order_detail
+from .views import master_detail, thanks, test, orders_list, order_detail
+# Эти маршруты будут доступны с префиксом /barbershop/
 
 urlpatterns = [
     path('masters/<int:master_id>/', master_detail),
     path('thanks/', thanks, name='thanks'),
     path('orders/', orders_list, name='orders_list'),
     path('orders/<int:order_id>/', order_detail, name='order_detail'),
+
+    path('test/', test),
+
 ]
