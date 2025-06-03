0f683b09826618cb075a3fc493f85bc6c2aaf558
Semyon <semenb668@gmail.com>
2025-06-03 17:52:51 +0600

    lesson_48: добавлена модель Order с полями для хранения информации о заказах и создана миграция для её добавления в базу данных; обновлены заметки по работе с ORM.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    client_name = models.CharField(max_length=100)
+    services = models.CharField(max_length=200)
+    master_id = models.IntegerField()
+    date = models.DateField()
+    status = models.CharField(max_length=50)
