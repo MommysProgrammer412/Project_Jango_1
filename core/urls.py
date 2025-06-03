a36dc654b9a086d6023c1baec02651f9756ee2d2
Semyon <semenb668@gmail.com>
2025-06-03 14:03:44 +0600

    hw_38: Цветовое оформление статусов заявок, проверка маршрутов, страница деталей заказа

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,12 @@
 # core/urls.py
 from django.contrib import admin
 from django.urls import path
-from .views import master_detail, thanks, test, orders_list, order_detail
-# Эти маршруты будут доступны с префиксом /barbershop/
+from .views import landing, master_detail, thanks, test, orders_list, order_detail
 
 urlpatterns = [
-    path('masters/<int:master_id>/', master_detail),
+    path('', landing, name='landing'),
     path('thanks/', thanks, name='thanks'),
     path('orders/', orders_list, name='orders_list'),
     path('orders/<int:order_id>/', order_detail, name='order_detail'),
-
     path('test/', test),
-
 ]
