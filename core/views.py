b50b588b7134c8ffc496eb63bb23629223c910d7
Semyon <semenb668@gmail.com>
2025-06-03 19:13:53 +0600

    lesson_48: добавлен список заказов и детали заказа с использованием модели Order; обновлены заметки по работе с ORM.

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .data import *
 from django.contrib.auth.decorators import login_required
+from .models import Order
 
 
 def landing(request):
@@ -110,12 +111,14 @@ def test(request):
 
 @login_required
 def orders_list(request):
+    orders = Order.objects.all()
     context = {"orders": orders, "title": "Список заказов"}
     return render(request, "core/orders_list.html", context)
 
 @login_required
 def order_detail(request, order_id: int):
     try:
+        order = Order.objects.get(id=order_id)
         order = [o for o in orders if o["id"] == order_id][0]
     except IndexError:
         # Если заказ не найден, возвращаем 404 - данные не найдены
