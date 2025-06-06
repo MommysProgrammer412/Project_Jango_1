2d18080c0cb2aa6306b13d0b4955ac789038f33b
Semyon <semenb668@gmail.com>
2025-06-06 20:23:44 +0600

    hw_39: регистрация моделей в админке

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
-from .models import Master, Order, Service
+from .models import Order, Master, Service, Review
 
-# регистрация в одну строку в админке 
-admin.site.register(Master)
-admin.site.register(Order)
-admin.site.register(Service)
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """Административная панель для модели Service"""
+    list_display = ['name', 'price', 'duration', 'is_popular']
+    list_filter = ['is_popular']
+    search_fields = ['name', 'description']
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    """Административная панель для модели Master"""
+    list_display = ['name', 'phone', 'experience', 'is_active']
+    list_filter = ['is_active', 'experience']
+    search_fields = ['name', 'phone']
+    filter_horizontal = ['services']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Административная панель для модели Order"""
+    list_display = ['client_name', 'phone', 'status', 'master', 'appointment_date', 'date_created']
+    list_filter = ['status', 'date_created', 'appointment_date']
+    search_fields = ['client_name', 'phone', 'comment']
+    filter_horizontal = ['services']
+    readonly_fields = ['date_created', 'date_updated']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Административная панель для модели Review"""
+    list_display = ['client_name', 'master', 'rating', 'created_at', 'is_published']
+    list_filter = ['rating', 'is_published', 'created_at']
+    search_fields = ['client_name', 'text']
+    readonly_fields = ['created_at']
