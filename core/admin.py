d0910d642926bb2f511833cf5f0c17184b9ed896
Semyon <semenb668@gmail.com>
2025-06-17 15:05:06 +0600

    lesson_50: удален неиспользуемый маршрут и класс Employee из views.py для упрощения кода

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Master, Order, Service
+from .models import Order, Master, Service
 
-# регистрация в одну строку в админке 
+# регистрация в одну строку в админке
+admin.site.register(Order) 
 admin.site.register(Master)
-admin.site.register(Order)
 admin.site.register(Service)
